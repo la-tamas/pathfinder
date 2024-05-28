c301c7e3ce927b0296e51ebe51131020719efa2c
LATamas <tamas.albert.laczko@gmail.com>
2024-05-28 21:58:28 +0300

    Resolve ios change issues

--- src/components/navbar/navbar.wrapper.tsx
+++ src/components/navbar/navbar.wrapper.tsx
@@ -1,9 +1,10 @@
-import { forwardRef, MouseEvent as ReactMouseEvent } from 'react'
+import { forwardRef, ChangeEventHandler } from 'react'
 import { GrPowerReset } from 'react-icons/gr'
 import { BiHelpCircle } from 'react-icons/bi'
 import { algorithmNames, generatorNames } from '../../algorithms'
 import useGridActions from '../../hooks/useGridActions'
-import { AlgorithmTypes } from '../../context/GridContext'
+import { AlgorithmTypes, GeneratorTypes } from '../../context/GridContext'
+import { eventNames } from 'process'
 
 const algorithms = Object.entries(algorithmNames)
 
@@ -17,9 +18,9 @@ const NavbarWrapper = forwardRef<HTMLDivElement, NavbarWrapperProps>((props, ref
     const { onReset } = props
     const { resolve } = useGridActions()
 
-    const handleClick = (e: ReactMouseEvent<HTMLOptionElement, MouseEvent>, algorithm: AlgorithmTypes) => {
-        e.preventDefault()
-        resolve(algorithm)
+    const handleChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+        event.preventDefault()
+        resolve(event.currentTarget.value as AlgorithmTypes | GeneratorTypes)
     }
 
     const handleReset = () => {
@@ -40,28 +41,32 @@ const NavbarWrapper = forwardRef<HTMLDivElement, NavbarWrapperProps>((props, ref
                 >
                     <GrPowerReset size={22} color="white" style={{ color: 'white' }} />
                 </button>
-                <select value="none" aria-readonly className="appearance-none bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 mr-1 border border-gray-300 text-gray-900 text-sm rounded block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+                <select
+                    onChange={handleChange}
+                    className="appearance-none bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 mr-1 border border-gray-300 text-gray-900 text-sm rounded block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                >
                     <option selected value="none" hidden>Solve</option>
                     {
                         algorithms.map((item, index) => (
                             <option key={`algorithm-${index}`}
                                 className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 mr-1 min-w-8 rounded text-ellipsis"
                                 value={item[0]}
-                                onClick={(e) => handleClick(e, item[0] as AlgorithmTypes)}
                             >
                                 {item[1]}
                             </option>
                         ))
                     }
                 </select>
-                <select value="none" aria-readonly className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 mr-1 border border-gray-300 text-gray-900 text-sm rounded block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+                <select
+                    onChange={handleChange}
+                    className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 mr-1 border border-gray-300 text-gray-900 text-sm rounded block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                >
                     <option selected value="none" hidden>Generate</option>
                     {
                         generators.map((item, index) => (
                             <option key={`generate-${index}`}
                                 className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 mr-1 min-w-8 rounded text-ellipsis"
                                 value={item[0]}
-                                onClick={(e) => handleClick(e, item[0] as AlgorithmTypes)}
                             >
                                 {item[1]}
                             </option>
